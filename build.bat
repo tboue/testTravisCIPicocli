39a98890a71a53329d39dfc93df3446e3467aad4
tboue <@microej.com>
2021-04-15 15:58:45 +0200

    graal windows 21

--- build.bat
+++ build.bat
@@ -1,7 +1,2 @@
 echo "test"
-echo on
 
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars64.bat" 
-
-java -agentlib:native-image-agent=config-output-dir=META-INF/native-image -jar primeDecomposition.jar
-%HOME%/.sdkman/candidates/java/current/bin/native-image.cmd --verbose --static --no-fallback -H:+ReportExceptionStackTraces -jar primeDecomposition.jar 
