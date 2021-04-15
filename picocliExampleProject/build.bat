a2a8ffc46ee985dbdc7690b5605b31108829f3d5
tboue <@microej.com>
2021-04-15 17:49:12 +0200

    graal windows 30

--- picocliExampleProject/build.bat
+++ picocliExampleProject/build.bat
@@ -2,5 +2,6 @@ echo "test"
 cd executables
 java -agentlib:native-image-agent=config-output-dir=META-INF/native-image -jar primeDecomposition.jar
 ls -R
-native-image.cmd --verbose  --no-fallback -H:+ReportExceptionStackTraces -jar primeDecomposition.jar 
+java -jar primeDecomposition.jar -n 4543543543
+native-image.cmd --verbose --static --no-fallback -H:+ReportExceptionStackTraces -jar primeDecomposition.jar 
 echo "ended"
