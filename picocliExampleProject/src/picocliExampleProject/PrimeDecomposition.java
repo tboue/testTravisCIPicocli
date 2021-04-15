033a02d7ce8a46238536ca7c4cffbe2def494cf3
tboue <@microej.com>
2021-04-15 14:51:43 +0200

    graal windows 9

--- picocliExampleProject/src/picocliExampleProject/PrimeDecomposition.java
+++ picocliExampleProject/src/picocliExampleProject/PrimeDecomposition.java
@@ -9,12 +9,9 @@ import picocli.CommandLine.Option;
 @Command(name = "primeDecomposition", mixinStandardHelpOptions = true, version = "checksum 4.0", description = "Prints the prime Decomposition of the given parameter")
 class PrimeDecomposition implements Callable<Integer> {
 
-	@Option(names = { "-n", "--number" }, description = "The integer to test")
-	private long number = 0;
-
-	// Takes number as parameter, better but can't launch from eclipse
-	// @Parameters(index = "0", description = "The integer to test")
-	// private int number = 0;
+	 Takes number as parameter, better but can't launch from eclipse
+	 @Parameters(index = "0", description = "The integer to test")
+	 private int number = 0;
 
 	@Override
 	public Integer call() throws Exception {
