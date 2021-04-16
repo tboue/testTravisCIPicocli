ba2cb83aa81524bae7f69cd584af6dc68d518bc2
tboue <@microej.com>
2021-04-16 17:16:03 +0200

    Upload with checksum

--- picocliExampleProject/build.bat
+++ picocliExampleProject/build.bat
@@ -7,3 +7,6 @@ java -jar primeDecomposition.jar 4543543543
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
 
 native-image.cmd --verbose --static --no-fallback -H:+ReportExceptionStackTraces -jar primeDecomposition.jar 
+
+sha256sum primeDecomposition.jar > sha256sumJar.txt
+sha256sum primeDecomposition.exe > sha256sumExe.txt
