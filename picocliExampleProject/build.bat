3f637807c3e3a7a95eb6ece50dd537cf51ea4a5b
tboue <@microej.com>
2021-04-16 17:25:43 +0200

    Upload with checksum 2

--- picocliExampleProject/build.bat
+++ picocliExampleProject/build.bat
@@ -1,4 +1,4 @@
-echo "test"
+echo "graal build started"
 cd executables
 java -agentlib:native-image-agent=config-output-dir=META-INF/native-image -jar primeDecomposition.jar
 ls -R
@@ -7,6 +7,4 @@ java -jar primeDecomposition.jar 4543543543
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build\vcvars64.bat"
 
 native-image.cmd --verbose --static --no-fallback -H:+ReportExceptionStackTraces -jar primeDecomposition.jar 
-
-sha256sum primeDecomposition.jar > sha256sumJar.txt
-sha256sum primeDecomposition.exe > sha256sumExe.txt
+echo "graal build ended"
